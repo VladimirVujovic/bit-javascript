9f3b7683159013903c6070319833c767202b679e
BIT student <classroom@bgit.rs>
2018-05-18 16:54:15 +0200

    MovieFormW5d3,4,5

---
+++ exerciseForFun.js
@@ -0,0 +1,15 @@
+var a = {x: 1, y: 2};
+
+function B (name,surname) {
+  this.name;
+  this.surname;
+//   this.f = function () {
+//     return 2;
+//   };
+}
+b = new B("kris","raic")
+B.prototayp.f = function(){
+        return 2;
+}
+
+console.
